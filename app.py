f136cf46fb9c36cab57413419524b67c6aacbeb1
unknown <eren.alltun@gmail.com>
2025-04-18 01:12:22 +0300

    prometheus grafana kurulumu

--- app.py
+++ app.py
@@ -3,20 +3,16 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-# Prometheus metric definition
 requests_total = Counter('requests_total', 'Total HTTP requests')
 
-# Ürünler ve sepet
 products = [{'id': 1, 'name': 'Telefon', 'price': 1000}, {'id': 2, 'name': 'Laptop', 'price': 5000}]
 cart = []
 
-# Ana sayfa
 @app.route('/')
 def home():
-    requests_total.inc()  # Her istek geldiğinde sayacı artır
+    requests_total.inc()
     return render_template('index.html', products=products)
 
-# Sepete ürün ekleme
 @app.route('/add/<int:product_id>')
 def add_to_cart(product_id):
     product = next((p for p in products if p['id'] == product_id), None)
@@ -25,7 +21,6 @@ def add_to_cart(product_id):
         return f"{product['name']} sepete eklendi!"
     return "Ürün bulunamadı."
 
-# Prometheus için metrikler
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
