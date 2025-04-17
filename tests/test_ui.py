5e962742648da0c62ddc906514834362a6481d4c
unknown <eren.alltun@gmail.com>
2025-04-17 22:59:10 +0300

    selenıum eklende projeye

---
+++ tests/test_ui.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def test_add_to_cart():
+    options = Options()
+    options.headless = True  # Tarayıcıyı başlatmadan işlemi gizlice yapar
+    driver = webdriver.Chrome(options=options)
+    driver.get('http://localhost:5000')  # Uygulamanızın URL'si
+    driver.find_element_by_xpath('//a[contains(@href, "/add/1")]').click()  # Ürün sepete ekleniyor
+    time.sleep(1)  # Sayfanın yüklenmesini beklemek için
+    assert "sepete eklendi" in driver.page_source  # Sepete eklendi mesajını kontrol et
+    driver.quit()
